325485c56dfe4c9c87eb611338b6fa635d6681de
Someone-who-wrote-it <130895603+Someone-who-wrote-it@users.noreply.github.com>
2023-04-16 12:19:19 +0300

    дописали змейку

--- snake.py
+++ snake.py
@@ -82,7 +82,35 @@ def gameloop():
                     y1_change = snake_block
                 if event.key == pygame.K_DOWN:
                     y1_change = -snake_block
+                    
+        if x1>= win_width or x1<0 or y1>=win_height or y1<0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        
         win.fill(blue)
+        
+        pygame.draw.rect(win,green,[foodx,foody,snake_block,snacke_block])
+        snake_Head=[]
+        sneaKe_Head.append(y1)
+        sneaKe_Head.append(x1)
+        sneaKe_Head.append(snake_Head)
+        
+        if len(snake_List) > Lenght_of_snake:
+            del snake_List[0]
+            
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        snake(snake_block,snake_List)
+        snake(Lenght_of_snake-1)
+        
+        pygame.display.update()
+        
+        if x1 == foodx and y1 == foody:
+            foodx, foody = apple()
+            Lengh_of_snake +=1
         
         pygame.display.update()
         clock.tick(snake_speed)
